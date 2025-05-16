180d671e0934d7a25a90744367cddd2cfb2c5f92
ly <564066938@qq.com>
2025-05-16 15:45:49 +0800

    创建仓库

---
+++ src/main/java/com/liyang/job/LyJobApplication.java
@@ -0,0 +1,13 @@
+package com.liyang.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyJobApplication.class, args);
+    }
+
+}
